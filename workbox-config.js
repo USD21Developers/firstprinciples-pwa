d939503952fc7e815ea5ef3c208f55398a1479e9
Jason McNeill <vrtjason@gmail.com>
2019-12-02 21:37:41 -0700

    Update SW

---
+++ workbox-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "globDirectory": ".",
+  "globPatterns": [
+    "**/*.{css,js,json,svg,woff,woff2,png,ico,html}",
+  ],
+  "globIgnores": [
+    "**/lang/**",
+    "**/fontawesome-free-5.11.2-web/**",
+    "**/high-res.png",
+    "**/img/body.png",
+    "**/icons/**",
+    "**/splashscreens/**",
+    "**/scripture.js",
+    "**/localforage.**",
+    "**/browser-sync.js",
+    "**/node_modules/**",
+    "**/composer.json",
+    "**/package*",
+    "**/workbox-config.js",
+    "**/light-darkness.css",
+    "**/the-church.css",
+    "**/body.png",
+    "**/icons/**",
+    "**/scripture.js",
+    "**/localforage*"
+  ],
+  "swDest": "sw.js"
+};
