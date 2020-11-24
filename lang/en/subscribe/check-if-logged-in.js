f65e6bc0da8fce07d2b6b164cd55279c3a584c74
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-11-23 23:09:49 -0700

    Remove obsolete code

--- lang/en/subscribe/check-if-logged-in.js
+++ lang/en/subscribe/check-if-logged-in.js
@@ -17,8 +17,7 @@
   if (!refreshToken.length) {
     const lang = getLang();
     const redirectUrl = `/lang/${lang}/subscribe/logout/`;
-    console.log(`No refresh token found.  Redirecting...`);
-    // return window.location.href = redirectUrl;
+    return window.location.href = redirectUrl;
   }
 
   if (!accessToken) {
