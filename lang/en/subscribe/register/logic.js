ece7c10f04117340bd3bd8f19d63ae25fa1a8f79
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-11-25 00:53:20 -0700

    Import text from content.xml

--- lang/en/subscribe/register/logic.js
+++ lang/en/subscribe/register/logic.js
@@ -6,7 +6,22 @@ async function onSubmit(e) {
   const lastname = e.target["lastname"].value.trim();
   const email = e.target["email"].value.trim();
 
+  // Set phrase IDs
+  const formIncomplete = 13;
+  const usernameRequired = 14;
+  const passwordRequired = 15;
+  const firstNameRequired = 16;
+  const lastNameRequired = 17;
+  const emailRequired = 18;
+
+  // Close modal if it is open
+  $("#modal1").modal("close");
   
+  if (!username.length) return showError(usernameRequired, formIncomplete);
+  if (!password.length) return showError(passwordRequired, formIncomplete);
+  if (!firstname.length) return showError(firstNameRequired, formIncomplete);
+  if (!lastname.length) return showError(lastNameRequired, formIncomplete);
+  if (!email.length) return showError(emailRequired, formIncomplete);
 }
 
 function attachListeners() {
