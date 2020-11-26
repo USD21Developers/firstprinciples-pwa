5935a8b7b5ec59783b6485ec5646f4b60097dd14
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-11-26 15:01:42 -0700

    Update registration

--- lang/en/subscribe/register/logic.js
+++ lang/en/subscribe/register/logic.js
@@ -4,6 +4,7 @@ async function onSubmit(e) {
   const password = e.target["password"].value.trim();
   const firstname = e.target["firstname"].value.trim();
   const lastname = e.target["lastname"].value.trim();
+  const fullname = e.target["fullname"].value.trim();
   const email = e.target["email"].value.trim();
 
   // Set phrase IDs
@@ -12,16 +13,43 @@ async function onSubmit(e) {
   const passwordRequired = 15;
   const firstNameRequired = 16;
   const lastNameRequired = 17;
-  const emailRequired = 18;
+  const fullNameRequired = 17;
+  const emailRequired = 19;
 
   // Close modal if it is open
   $("#modal1").modal("close");
   
+  // Validate
   if (!username.length) return showError(usernameRequired, formIncomplete);
   if (!password.length) return showError(passwordRequired, formIncomplete);
   if (!firstname.length) return showError(firstNameRequired, formIncomplete);
   if (!lastname.length) return showError(lastNameRequired, formIncomplete);
+  if (!fullname.length) return showError(fullNameRequired, formIncomplete);
   if (!email.length) return showError(emailRequired, formIncomplete);
+
+  const endpoint = window.location.hostname === "localhost" ? "http://localhost:4000/fp/register" : "https://api.usd21.org/fp/register";
+  fetch(endpoint, {
+    mode: "cors",
+    method: "POST",
+    body: JSON.stringify({
+      username: username,
+      password: password,
+      firstname: firstname,
+      lastname: lastname,
+      fullname: fullname,
+      email: email
+    }),
+    headers: new Headers({
+      "Content-Type": "application/json"
+    })
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => {
+      console.error(err);
+    });
 }
 
 function attachListeners() {
