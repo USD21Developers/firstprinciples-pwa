aac56a30c96088ce30d017bc9a814aa604ff6136
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-12-12 13:36:54 -0700

    Add enforceaccess.js

---
+++ lang/en/account/enforceaccess.js
@@ -0,0 +1,69 @@
+function getAPIHost(forceRemote = false) {
+  const local = "http://localhost:4000";
+  const remote = "https://api.usd21.org";
+  const host = window.location.hostname === "localhost" ? local : remote;
+  return forceRemote ? remote : host;
+}
+
+function getLang() {
+  const lang = window.location.pathname.substring(6, 8);
+  return lang;
+}
+
+function getAccessToken() {
+  let needToRefresh = false;
+  const accessToken = sessionStorage.getItem("accessToken") || "";
+  const now = Date.now().valueOf() / 1000;
+  let expiry = now;
+  try {
+    expiry = JSON.parse(atob(accessToken.split(".")[1])).exp;
+    if (expiry < now) needToRefresh = true;
+  } catch (err) {
+    needToRefresh = true;
+  }
+  return new Promise((resolve, reject) => {
+    if (!needToRefresh) return resolve(accessToken);
+    const refreshToken = localStorage.getItem("refreshToken") || "";
+    if (!refreshToken.length) return reject("refresh token missing");
+
+    const endpoint = `${getAPIHost()}/fp/refresh-token`;
+
+    fetch(endpoint, {
+      mode: "cors",
+      method: "POST",
+      body: JSON.stringify({
+        refreshToken: refreshToken,
+      }),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const logoutUrl = `/lang/${getLang()}/account/logout/`;
+        switch (data.msg) {
+          case "tokens renewed":
+            const { accessToken, refreshToken } = data;
+            localStorage.setItem("refreshToken", refreshToken);
+            sessionStorage.setItem("accessToken", accessToken);
+            resolve(accessToken);
+            break;
+          default:
+            window.location.href = logoutUrl;
+            break;
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+}
+
+function verifyRefreshToken() {
+  const logoutUrl = `/lang/${getLang()}/account/logout/`;
+  const refreshToken = localStorage.getItem("refreshToken") || "";
+  const isAuthorized = refreshToken.length;
+  if (!isAuthorized) window.location.href = logoutUrl;
+}
+
+verifyRefreshToken(logoutUrl);
