0f82878e4b999135926a4376fa330ef0d785f82b
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2020-12-30 14:27:13 -0700

    Add spinner while content loads

--- lang/en/account/why/logic.js
+++ lang/en/account/why/logic.js
@@ -1,3 +1,10 @@
+function toggleSpinner() {
+  const spinner = document.querySelector("#spinner");
+  const content = document.querySelector("main");
+
+  hideSpinner(content, spinner);
+}
+
 function toggleGoBackLink() {
   const goBackLink = document.querySelector("[href='javascript:history.go(-1)']");
   if (history.length) goBackLink.classList.remove("hide");
@@ -5,6 +12,7 @@ function toggleGoBackLink() {
 
 async function init() {
   await showPhrases();
+  toggleSpinner();
   toggleGoBackLink();
 }
 
