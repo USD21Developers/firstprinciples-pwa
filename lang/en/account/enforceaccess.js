ef647fbb15b0599ce5097e15fa1433fc709b58a1
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2021-01-03 21:06:47 -0700

    Move function into appropriate file

--- lang/en/account/enforceaccess.js
+++ lang/en/account/enforceaccess.js
@@ -59,6 +59,20 @@ function getAccessToken() {
   });
 }
 
+function isSubscriptionActive() {
+  const subscriptionToken = localStorage.getItem("subscriptionToken") || "";
+
+  if (!subscriptionToken.length) return false;
+
+  const now = moment().utc().unix();
+  const expiry = parseInt(JSON.parse(atob(subscriptionToken.split(".")[1])).exp) || 0;
+  const isSubscribed = (now < expiry) || false;
+
+  if (!isSubscribed) return false;
+
+  return true;
+}
+
 function verifyRefreshToken() {
   const logoutUrl = `/lang/${getLang()}/account/logout/`;
   const refreshToken = localStorage.getItem("refreshToken") || "";
