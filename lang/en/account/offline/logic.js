3b2ef3de152a026dde67c6301d7e7446c4963959
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2021-01-04 17:34:41 -0700

    Set destination of Continue link for offline page

--- lang/en/account/offline/logic.js
+++ lang/en/account/offline/logic.js
@@ -5,9 +5,24 @@ function toggleSpinner() {
   hideSpinner(content, spinner);
 }
 
+function goBack(e) {
+  e.preventDefault();
+  const cameFromAnAccountPage = (document.referrer.indexOf("/account/") >= 0) || false;
+  if (cameFromAnAccountPage) {
+    window.location.href = document.referrer;
+  } else {
+    window.location.href = e.target.href;
+  }
+}
+
+function attachEventListeners() {
+  document.querySelector("#continuelink").addEventListener("click", goBack);
+}
+
 async function init() {
   await showPhrases();
   toggleSpinner();
+  attachEventListeners();
 }
 
 init();
