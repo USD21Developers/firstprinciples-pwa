2ab1c1fb3042dd8af8204fb96cd8c1d97fac5545
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2021-01-24 13:46:08 -0700

    Fix bug

--- lang/en/account/admin/coupons/logic.js
+++ lang/en/account/admin/coupons/logic.js
@@ -14,7 +14,10 @@ function populateMetaData(data, country="us") {
   numDiscontinued.innerHTML = (num_discontinued.toString().length) ? num_discontinued : `&mdash;`;
   numRedeemed.innerHTML = (num_redeemed.toString().length) ? num_redeemed : `&mdash;`;
   cumulativeAmount.innerHTML = (country === "us") ? `$${Number(cumulative_amount || 0).toLocaleString(country)} <span style="font-variant:small-caps;">usd</span>` : `$ ${Number(cumulative_amount || 0).toLocaleString(country)} <span style="font-variant:small-caps;">usd</span>`;
-  mostRecentRedemptionDate.innerHTML = (most_recent_redemption_date) ? moment.utc("2021-01-19 15:31").format("YYYY-MM-DD") : `&mdash;`;
+
+  const mostRecentRedemptionDateFormatted = (most_recent_redemption_date) ? moment(most_recent_redemption_date).format("YYYY/MM/DD") : null;
+  const mostRecentRedemptionDateLs = (mostRecentRedemptionDateFormatted) ? new Intl.DateTimeFormat(country, { dateStyle: "long" }).format(new Date(mostRecentRedemptionDateFormatted)) : `&mdash;`;
+  mostRecentRedemptionDate.innerHTML = (mostRecentRedemptionDateLs.length) ? mostRecentRedemptionDateLs : `&mdash;`;
   if (most_recent_couponcode && most_recent_couponcode.length) {
     mostRecentRedemptionDate.setAttribute("data-tooltip", most_recent_couponcode);
     mostRecentRedemptionDate.setAttribute("data-position", "bottom");
@@ -384,15 +387,15 @@ async function onAdd(e) {
     });
 }
 
-function showCouponList(data, country) {
+function showCouponList(data, country="us") {
   const couponlist = document.querySelector("#couponlist");
   let html = "";
   
   data.forEach(item => {
     const { couponid, couponcode, expiry, discountpercent, isdiscontinued, issuer } = item;
     const { userid, name } = issuer;
-    const expiry = moment.unix(subscriptionToken.exp).format("YYYY/MM/DD");
-    const lsExpiry = new Intl.DateTimeFormat(country, { dateStyle: "long" }).format(new Date(expiry));
+    const formattedExpiry = moment(expiry).format("YYYY/MM/DD");
+    const lsExpiry = new Intl.DateTimeFormat(country, { dateStyle: "long" }).format(new Date(formattedExpiry));
 
     html += `
       <li>
