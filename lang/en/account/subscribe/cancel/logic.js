8f7ee90c289413b44841fdbe6458d140c9a04412
Jason McNeill <jason.mcneill@grindstonewebdev.com>
2021-02-16 10:58:14 -0700

    Update subscription activation cancellation page

--- lang/en/account/subscribe/cancel/logic.js
+++ lang/en/account/subscribe/cancel/logic.js
@@ -1,6 +1,13 @@
+function toggleSpinner() {
+  const content = document.querySelector("main");
+  const spinner = document.querySelector("#spinner");
+  hideSpinner(content, spinner);
+}
+
 async function init() {
   checkIfOffline();
   await showPhrases();
+  toggleSpinner();
 }
 
 init();
